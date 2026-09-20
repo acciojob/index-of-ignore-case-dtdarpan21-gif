4648b2ae740d2d99747706af558e8f9679204902
dtdarpan21-gif <dtdarpan21@gmail.com>
2026-09-20 17:38:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function indexOfIgnoreCase(s1, s2) {
+function indexOfIgnoreCase(s1, s2) 
+	let s1lower = s1lower.toLowercase();
+	let s2lower = s2lower.toLowercase();
+
+return s1lower .indexof(s2lower);
+	
   // write your code here
 }
 
